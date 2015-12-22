d6087dc5ac8ffdb8336ecd1789deef91bab7c03a
hlechner <hlechner@gmail.com>
2015-12-22 21:57:12 -0200

    initial aMule commit

---
+++ amule/src/libs/ec/cpp/ECVersion.h
@@ -0,0 +1,14 @@
+/* Automatically generated file. Do not edit! */
+
+#ifndef ECVERSION_H
+#define ECVERSION_H
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef SVNDATE
+#define EC_VERSION_ID "f7e1e202a07e557891c86b944b9c4264"
+#endif
+
+#endif /* ECVERSION_H */
